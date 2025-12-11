18c5f3e9ff3c8c1826e25b42c1711a3e4f5a52ba
GunaGGowda <150352153+GunaGGowda@users.noreply.github.com>
2025-12-11 19:35:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $("#regForm").submit(function (event) {
+        alert("Your registration form is being submitted...");
+        // No return false, no preventDefault
+    });
+});
